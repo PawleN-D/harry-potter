545842a6d9ce14d6b2987d106fa8a68e20651566
PawleN-D <sirpawle@gmail.com>
2020-03-11 21:38:45 +0200

    added input fields

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let characters = [
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "likes": ["Hogwarts", "Magic", "Hagrid"],
+        "dislikes": ["Voldemort", "Draco Malfoy", "Dementors"],
+    },
+
+    {
+        "firstName": "Ron",
+        "lastName": "Weasley",
+        "likes": ["Hogwarts", "Magic", "Quidditch"],
+        "dislikes": ["Voldemort", "Spiders", "Mrs Norris "],
+    },
+    {
+        "firstName": "Neville",
+        "lastName": "Longbottom",
+        "likes": ["Hogwarts", "Magic", "Luna Lovegood"],
+        "dislikes": ["Voldemort", "Moaning Myrtle", "Death-Eaters"],
+    },
+
+    {
+        "firstName": "Draco",
+        "lastName": "Malfoy",
+        "likes": ["Hogwarts", "Prof Snape", "Dark Magic"],
+        "dislikes": ["Dumbledore", "Muggles", "Harry Potter"],
+    },
+
+
+];
+
+console.log(characters[0].firstName);
+console.log(characters[0].likes[0]);
+
+// function lookUp(firstName, prop){
+//     for(let i =0; i < characters.length; i++ ){
+//         if() {
+            
+//         }
+//     }
+// }
+ //add at least 3 other harry potter characters here
+// Change these values to test your function
+// lookUp("Harry", "likes");
